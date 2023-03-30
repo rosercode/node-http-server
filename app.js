d6dd6446232e2f082d553fb9b672f5008e79a797
rosercode <rosercode@gmail.com>
2023-03-30 14:27:05 +0800

    feat: Add yargs library for command-line arguments parsing
    This commit introduces the yargs library to the project, which will be used for parsing command-line arguments. The library makes it easy to define and process command-line options and arguments in a structured way, improving the overall usability of the project.

--- app.js
+++ app.js
@@ -1,12 +1,48 @@
+const config = require("./config")
+const yargs = require('yargs');
+
+// 定义自定义选项和参数
+const argv = yargs
+  .option('port', {
+    description: 'Port number',
+    alias: 'p',
+    type: 'number',
+    default: config.PORT
+  })
+  .option('dir', {
+    description: 'dir',
+    alias: 'd',
+    type: 'string',
+    default: config.BASE_URL
+  })
+  .option('debug', {
+    description: 'debug',
+    type: 'boolean',
+    default: config.DEBUG
+  })
+  .argv;
+
 var ejs = require('ejs')
     , morgan = require('morgan')
     , express = require('express')
     , router = require('./router.js') 
 const app = express()
-const port = 3000
 
-// 日志打印
-app.use(morgan('short'));
+// 中间件函数
+const myMiddleware = (req, res, next) => {
+  req.dir = argv.dir;
+  next();
+};
+// 使用中间件
+app.use(myMiddleware);
+
+if (argv.debug) {
+  // 日志打印
+  app.use(morgan('short'));
+}else{
+  console.log = function() {};
+}
+
 //设置渲染文件的目录
 app.set('views', './views');
 //设置html模板渲染引擎
@@ -15,8 +51,6 @@ app.engine('html', ejs.__express);
 app.set('view engine', 'html');
 
 app.use(router)
-
-const config = require("./config")
 
 app.listen(config.PORT, () => {
   console.log(`Example app listening on port ${config.PORT}`)
--- router.js
+++ router.js
@@ -1,117 +1,115 @@
 var express = require('express')
-    ,fs = require('fs')
-    ,path = require('path');
-    
-var router = express.Router()
+  , fs = require('fs')
+  , path = require('path');
 
-var config = require("./config")
+var router = express.Router()
 
 //调用路由，进行页面渲染
 router.get('/*', function (req, res) {
-  
-    var filepath = decodeURI(config.BASE_URL + req.path.replace(/\//g, path.sep))
-    fs.exists(filepath, (exists) => {
-      if (exists) {
-  
-        fs.stat(filepath, function (err, data) {
-          if (data.isFile()) {
-  
-            //获取后缀
-            var ext = (function(){
-              var idx1 = filepath.lastIndexOf(".");
-              return filepath.substr(idx1 + 1);
-            })()
-  
-            var filename = (function(){
-              var idx2 = filepath.lastIndexOf(path.sep);
-              return req.path.substr(idx2)
-            })()
-  
-            if (isAssetTypeVideo(ext)) {
-              console.log('文件-视频文件');
-              if(req.query.play!= undefined){
-                  const range = req.headers.range;
-                  if (!range) {
-                      res.status(400).send("Requires Range header");
-                  }
-                  const videoPath = filepath;
-                  console.log('视频播放路径' + filepath);
-                  const videoSize = fs.statSync(videoPath).size;
-                  const CHUNK_SIZE = 10 ** 6;
-                  const start = Number(range.replace(/\D/g, ""));
-                  const end = Math.min(start + CHUNK_SIZE, videoSize - 1);
-                  const contentLength = end - start + 1;
-                  const headers = {
-                      "Content-Range": `bytes ${start}-${end}/${videoSize}`,
-                      "Accept-Ranges": "bytes",
-                      "Content-Length": contentLength,
-                      "Content-Type": "video/mp4",
-                  };
-                  res.writeHead(206, headers);
-                  const videoStream = fs.createReadStream(videoPath, { start, end });
-                  videoStream.pipe(res);
-  
-                }else{
-                    console.log('调用渲染模板');
-                    //调用渲染模板
-                    res.render('video', {
-                   //传参
-                        title: '首页', path: req.path.replace(/\//g, path.sep)
-                  });
-                }
-            }else{
-              console.log('文件-一般文件（不是视频文件）');
-              var rs = fs.createReadStream(filepath);
-              // 设置响应请求头，200表示成功的状态码，headers表示设置的请求头
-              res.writeHead(200);
-              // 将可读流传给响应对象resp
-              rs.pipe(res);
+  const dir = req.dir;
+  var filepath = decodeURI(dir + req.path.replace(/\//g, path.sep))
+  fs.exists(filepath, (exists) => {
+    if (exists) {
+
+      fs.stat(filepath, function (err, data) {
+        if (data.isFile()) {
+
+          //获取后缀
+          var ext = (function () {
+            var idx1 = filepath.lastIndexOf(".");
+            return filepath.substr(idx1 + 1);
+          })()
+
+          var filename = (function () {
+            var idx2 = filepath.lastIndexOf(path.sep);
+            return req.path.substr(idx2)
+          })()
+
+          if (isAssetTypeVideo(ext)) {
+            console.log('文件-视频文件');
+            if (req.query.play != undefined) {
+              const range = req.headers.range;
+              if (!range) {
+                res.status(400).send("Requires Range header");
+              }
+              const videoPath = filepath;
+              console.log('视频播放路径' + filepath);
+              const videoSize = fs.statSync(videoPath).size;
+              const CHUNK_SIZE = 10 ** 6;
+              const start = Number(range.replace(/\D/g, ""));
+              const end = Math.min(start + CHUNK_SIZE, videoSize - 1);
+              const contentLength = end - start + 1;
+              const headers = {
+                "Content-Range": `bytes ${start}-${end}/${videoSize}`,
+                "Accept-Ranges": "bytes",
+                "Content-Length": contentLength,
+                "Content-Type": "video/mp4",
+              };
+              res.writeHead(206, headers);
+              const videoStream = fs.createReadStream(videoPath, { start, end });
+              videoStream.pipe(res);
+
+            } else {
+              console.log('调用渲染模板');
+              //调用渲染模板
+              res.render('video', {
+                //传参
+                title: '首页', path: req.path.replace(/\//g, path.sep)
+              });
             }
-  
+          } else {
+            console.log('文件-一般文件（不是视频文件）');
+            var rs = fs.createReadStream(filepath);
+            // 设置响应请求头，200表示成功的状态码，headers表示设置的请求头
+            res.writeHead(200);
+            // 将可读流传给响应对象resp
+            rs.pipe(res);
           }
-  
-          if (data.isDirectory()) {
-            console.log('文件-目录文件');
-  
-            fs.readdir(filepath, function (err, files) {
-              for (let i = 0; i < files.length; i++) {
-                var file = files[i]
-                var stat = fs.statSync(config.BASE_URL + path.sep + decodeURI(req.path.replace(/\//g, path.sep)) + path.sep + file)
-                if (stat.isDirectory()) {
-                  files[i] = file + '/'
-                }
+
+        }
+
+        if (data.isDirectory()) {
+          console.log('文件-目录文件');
+
+          fs.readdir(filepath, function (err, files) {
+            for (let i = 0; i < files.length; i++) {
+              var file = files[i]
+              var stat = fs.statSync(dir + path.sep + decodeURI(req.path.replace(/\//g, path.sep)) + path.sep + file)
+              if (stat.isDirectory()) {
+                files[i] = file + '/'
               }
-  
-              if (err) {
-                console.log('Error', err);
-              } else {
-                new_path = ''
-                if(req.path != '/'){
-                  new_path =  decodeURI(req.path)
-                }
-  
-                //调用渲染模板
-                res.render('index', {
-                  //传参
-                  title: '首页', path: new_path, files: files
-                });
+            }
+
+            if (err) {
+              console.log('Error', err);
+            } else {
+              new_path = ''
+              if (req.path != '/') {
+                new_path = decodeURI(req.path)
               }
-            });
-  
-          }
-        })
-      } else {
-        console.log('文件路径不存在: ' + filepath);
-        res.send('文件路径不存在: ' + filepath);
-      }
-    })
-  });
-  
-  
+
+              //调用渲染模板
+              res.render('index', {
+                //传参
+                title: '首页', path: new_path, files: files
+              });
+            }
+          });
+
+        }
+      })
+    } else {
+      console.log('文件路径不存在: ' + filepath);
+      res.send('文件路径不存在: ' + filepath);
+    }
+  })
+});
+
+
 function isAssetTypeVideo(ext) {
-    return [
-      'mp4', 'flv', 'avi', 'mov', 'wmv', 'mkv'].
-      indexOf(ext.toLowerCase()) !== -1;
-  }
+  return [
+    'mp4', 'flv', 'avi', 'mov', 'wmv', 'mkv'].
+    indexOf(ext.toLowerCase()) !== -1;
+}
 
 module.exports = router
